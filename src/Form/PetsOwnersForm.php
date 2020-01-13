6007ede2292c33d1c93396d34782f4ffaad33bf3
Dmytro-Litvinchuk <dimawaw11@gmail.com>
2020-01-13 13:28:23 +0200

    Task 3

---
+++ src/Form/PetsOwnersForm.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\pets_owners_storage\Form;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\pets_owners\Form\FormPetsOwners;
+
+/**
+ * Class PetsOwnersForm.
+ *
+ * @package Drupal\pets_owners_storage\Form
+ */
+class PetsOwnersForm extends FormPetsOwners {
+
+  /**
+   * @inheritDoc
+   */
+  public function getFormId() {
+    return 'pets_owners_form';
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $pid = NULL) {
+    // Get values from DB.
+    $values = Database::getConnection()
+      ->select('pets_owners_storage', 'p')
+      ->fields('p', [
+        'pid',
+        'prefix',
+        'name',
+        'gender',
+        'age',
+        'father',
+        'mother',
+        'pets_name',
+        'email',
+      ])->condition('pid', $pid)
+      ->execute()->fetchAssoc();
+    // Used all fields of parent form.
+    $form = parent::buildForm($form, $form_state);
+    if (!empty($values)) {
+      $form['name']['#default_value'] = $values['name'];
+      $form['prefix']['#default_value'] = $values['prefix'];
+      $form['gender']['#default_value'] = $values['gender'];
+      $form['age']['#default_value'] = $values['age'];
+      $form['father']['#default_value'] = $values['father'];
+      $form['mother']['#default_value'] = $values['mother'];
+      $form['have_pets']['#default_value'] = 1;
+      $form['pets_name']['#default_value'] = $values['pets_name'];
+      $form['email']['#default_value'] = $values['email'];
+      // Pass value $pid to submit form.
+      $form_state->set('pid', $pid);
+    }
+    return $form;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $pid = $form_state->get('pid');
+    $query = \Drupal::database();
+    $query->update('pets_owners_storage')
+      ->fields([
+        'prefix' => $form_state->getValue('prefix'),
+        'name' => $form_state->getValue('name'),
+        'gender' => $form_state->getValue('gender'),
+        'age' => $form_state->getValue('age'),
+        'father' => $form_state->getValue('father'),
+        'mother' => $form_state->getValue('mother'),
+        'pets_name' => $form_state->getValue('pets_name'),
+        'email' => $form_state->getValue('email'),
+      ])
+      ->condition('pid', $pid)
+      ->execute();
+    $text = $this->t('The value changed');
+    \Drupal::messenger()->addMessage($text);
+    $form_state->setRedirect('pets_owners_storage.content');
+  }
+
+}
